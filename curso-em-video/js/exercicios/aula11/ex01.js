a7346b409f72c58940f7705034ccc723a8d5ba52
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2023-08-30 18:57:55 -0300

    inicio modulo d js

---
+++ curso-em-video/js/exercicios/aula11/ex01.js
@@ -0,0 +1,9 @@
+var vel = 61.0
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade. MULTADO!`)
+}
+
+console.log(`Use sempre o cinto!`)
---
+++ curso-em-video/js/exercicios/ex02.js
@@ -0,0 +1,9 @@
+var pais = 'BR'
+
+if (pais == 'BR'){
+    console.log('Você é brasileira')
+}
+
+else {
+    console.log('Você é estrangeiro')
+}
