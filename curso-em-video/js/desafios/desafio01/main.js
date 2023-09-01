2548111634ee536506d0004a1326c7a6b8f39c11
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2023-09-01 14:47:23 -0300

    desafio 2 modulo d js

--- curso-em-video/js/desafios/desafio01/main.js
+++ curso-em-video/js/desafios/desafio01/main.js
@@ -6,15 +6,15 @@ function alterando(){
     msg1.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 6 && hora < 12){
-        img.src = '/image/manha.png'
+        img.src = 'image/manha.png'
         document.body.style.background = '#e2cd9f'
 
     } else if (hora > 12 && hora < 18){
-        img.src = '/image/tarde.png'
+        img.src = 'image/tarde.png'
         document.body.style.background = '#b9846f'
  
     } else {
-        img.src = '/image/noite.png'
+        img.src = 'image/noite.png'
         document.body.style.background = '#515154'
     }
 }
